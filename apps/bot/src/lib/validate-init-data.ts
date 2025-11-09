1e5051a95487516b384af5f7251442e44df44a92
Andrew-Sem <avvyayvayva@gmail.com>
2025-11-09 20:07:32 +0300

    refactor: remove unused util

--- apps/bot/src/lib/validate-init-data.ts
+++
@@ -1,6 +0,0 @@
-import { type ValidateValue, validate } from "@telegram-apps/init-data-node";
-import { env } from "../env";
-
-export const validateInitData = (value: ValidateValue) => {
-	validate(value, env.BOT_TOKEN);
-};
